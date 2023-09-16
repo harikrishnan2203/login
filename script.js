38cb5e3c51062a100cd36da1dd951cf64aa287e7
harikrishnan2203 <136174723+harikrishnan2203@users.noreply.github.com>
2023-09-16 14:57:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function foo(){
+    var first = document.getElementById("fName").value;
+    var middle = document.getElementById("mName").value;
+    var last = document.getElementById("lName").value;
+    var email = document.getElementById("mail").value;
+    var pin = document.getElementById("pin").value;
+
+    console.log(first);
+    console.log(middle);
+    console.log(last);
+    console.log(email);
+    console.log(pin);
+}
